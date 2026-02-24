3bb3a9b73f8227852cfa60fd736db7c0a305808e
Chase <chaseandrewstephens2@gmail.com>
2026-02-24 12:58:33 -0600

    prediction algorithm skeleton

--- Prediciton Algorithm/prediction_alg.py
+++ Prediciton Algorithm/prediction_alg.py
@@ -1,4 +1,44 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn import tree
+from sklearn import metrics
+from google.colab import drive
+
+drive.mount('/content/drive')
+
 path = '/content/drive/MyDrive/cs167/breast-cancer-wisconsin-data.csv'
 data= pd.read_csv(path)
 data.head()
+
+'''
+#fix later, drop null value
+data.drop('Unnamed: 32', inplace=True, axis=1)
+data.isna().any()
+
+target = "diagnosis"
+predictors = data.columns.drop(target) #gets all of the columns except the target
+
+train_data, test_data, train_sln, test_sln = train_test_split(data[predictors], data[target], test_size = 0.2, random_state=41)
+
+
+dt = tree.DecisionTreeClassifier()
+dt.fit(train_data,train_sln)
+predictions = dt.predict(test_data)
+
+print("accuracy score: ", metrics.accuracy_score(test_sln,predictions))
+vals = data[target].unique() ## possible classification values (M = malignant; B = benign)
+conf_mat = metrics.confusion_matrix(test_s`ln, predictions, labels=vals)
+print(pd.DataFrame(conf_mat, index = "True " + vals, columns = "Predicted " + vals))
+
+forest = RandomForestClassifier(n_estimators = 200, random_state = 0)
+forest.fit(train_data,train_sln)
+predictions = forest.predict(test_data)
+print("accuracy score: ", metrics.accuracy_score(test_sln,predictions))
+
+vals = data[target].unique() ## possible classification values (M = malignant; B = benign)
+conf_mat = metrics.confusion_matrix(test_sln, predictions, labels=vals)
+print(pd.DataFrame(conf_mat, index = "True " + vals, columns = "Predicted " + vals))
+'''
